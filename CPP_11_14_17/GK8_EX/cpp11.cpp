4e894d8c0cbc82a0243f2bebdd01b2ece4c25eca
yoniavn <yoniavn@gmail.com>
2021-03-20 23:46:09 +0200

    Q1,Q2

--- CPP_11_14_17/GK8_EX/cpp11.cpp
+++ CPP_11_14_17/GK8_EX/cpp11.cpp
@@ -1,5 +1,6 @@
-/* 
-    C++11 Home assignment
+// C++ Home assignment
+
+/*    
     1. Implement function that gets integer numbers as parameter and return the sum 
         ** the function can be called with any number of parameters (not inside container, but as parameter) **
         std::cout << sum(2,5,10); //17
@@ -18,26 +19,83 @@
 
 using namespace std;
 
-//template function
+//****************************************First Question*************************************************
+//recursive function overload with single argument to terminate the recursion
 template <typename T>
-T sum(const T &num)
+T Generic_Sum(const T &num)
 {
     return num;
 }
 
+//function that gets numbers as argument and recursively calculate and return the sum of those numbers
 template <typename T, typename... ARGS>
-T sum(const T &num, const ARGS &...numbers)
+T Generic_Sum(const T &num, const ARGS &...numbers)
 {
-    return num + sum(numbers...);
+    return num + Generic_Sum(numbers...);
+}
+//********************************************************************************************************
+
+//****************************************Second Question*************************************************
+
+template <typename T, size_t N>
+array<T, N / 2> Sum_and_Shrink(array<T, N> &inputArr)
+{
+    array<T, N / 2> result{};
+
+    if (!inputArr.size())
+    {
+        return result;
+    }
+
+    for (size_t i = 0; i < N / 2; ++i)
+    {
+        result[i] = Generic_Sum(inputArr[2 * i], inputArr[2 * i + 1]);
+
+        //result[i] = inputArr[2 * i] + inputArr[2 * i + 1];
+    }
+    return result;
+}
+//********************************************************************************************************
+
+//print array elements
+template <typename T, size_t N>
+void PrintArray(array<T, N> &arr)
+{
+    cout << "[ ";
+    for (T &i : arr)
+        cout << i << " ";
+    cout << " ]\n";
 }
 
 int main()
 {
-    std::cout << sum(2, 5, 10) << endl;                           //17
-    std::cout << sum(2, 5, 10, 2, 5, 102, 5, 102, 5, 10) << endl; //248
-    std::cout << sum(3) << endl;
+    cout << Generic_Sum(2, 5, 10) << endl;                           //17
+    cout << Generic_Sum(2, 5, 10, 2, 5, 102, 5, 102, 5, 10) << endl; //248
+    cout << Generic_Sum(3) << endl;                                  //3
+    //cout << Generic_Sum(3.3, 5.5) << endl;                        //8.8
 
-    std::cout << sum(3.3, 5.5) << endl; //8.8
+    array<int, 6> MyArray = {1, 2, 100, 150, 200, 700};
+    array<int, 6> MyArray2 = {1, 2, 3, 4, 5, 6};
 
+    array<int, 0> MyArray3;
+
+    array<int, 4> MyArray4 = {1, 2, 3, 4};
+
+    array<int, 2> MyArray5 = {1, 2};
+
+    auto resultArray = Sum_and_Shrink(MyArray);
+    PrintArray(resultArray);
+
+    auto resultArray2 = Sum_and_Shrink(MyArray2);
+    PrintArray(resultArray2);
+
+    auto resultArray3 = Sum_and_Shrink(MyArray3);
+    PrintArray(resultArray3);
+
+    auto resultArray4 = Sum_and_Shrink(MyArray4);
+    PrintArray(resultArray4);
+
+    auto resultArray5 = Sum_and_Shrink(MyArray5);
+    PrintArray(resultArray5);
     return 0;
 }
